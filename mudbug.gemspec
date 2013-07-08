ca1b05d9ea51d20b52edd6d3d2808ade801ea176
Rick <rick@cloudscaling.com>
2013-07-07 21:50:23 -0700

    using gemspec file with Buildar 1.4

---
+++ mudbug.gemspec
@@ -0,0 +1,24 @@
+Gem::Specification.new do |s|
+  s.name        = 'mudbug'
+  s.summary     = "This hardy creature consumes JSON / REST APIs"
+  s.author      = "Rick Hull"
+  s.homepage    = "http://github.com/rickhull/mudbug"
+  s.license     = 'LGPL'
+  s.has_rdoc    = true
+  s.description = "GET, POST, PUT, and DELETE JSON payloads. Easy Accept headers. Fine-grained response handling using Mudbug#resource."
+
+  s.add_runtime_dependency  "rest-client", ["~> 1"]
+  s.add_runtime_dependency         "json", ["~> 1"]
+  s.add_runtime_dependency        "lager", [">= 0.2"]
+  s.add_development_dependency "minitest", [">= 0"]
+  s.add_development_dependency  "buildar", ["~> 1.4"]
+
+  # dynamic setup
+  this_dir = File.expand_path('..', __FILE__)
+  version_file = File.join(this_dir, 'VERSION')
+  manifest_file = File.join(this_dir, 'MANIFEST.txt')
+
+  # dynamic assignments
+  s.version  = File.read(version_file).chomp
+  s.files = File.readlines(manifest_file).map { |f| f.chomp }
+end
--- rakefile.rb
+++ rakefile.rb
@@ -2,23 +2,10 @@ require 'buildar/tasks'
 require 'rake/testtask'
 
 Buildar.conf(__FILE__) do |b|
-  b.name = 'mudbug'
-  b.use_version_file  = true
-  b.version_filename  = 'VERSION'
-  b.use_manifest_file = true
-  b.manifest_filename = 'MANIFEST.txt'
-  b.use_git           = true
-  b.publish[:rubygems] =  true
-
-  b.gemspec.summary     = "This hardy creature consumes JSON / REST APIs"
-  b.gemspec.description = "GET, POST, PUT, and DELETE JSON payloads. Easy Accept headers. Fine-grained response handling using Mudbug#resource."
-  b.gemspec.author      = "Rick Hull"
-  b.gemspec.homepage    = "http://github.com/rickhull/mudbug"
-  b.gemspec.license     = 'LGPL'
-
-  b.gemspec.add_runtime_dependency  "rest-client", ["~> 1"]
-  b.gemspec.add_runtime_dependency         "json", ["~> 1"]
-  b.gemspec.add_runtime_dependency        "lager", [">= 0.2"]
-  b.gemspec.add_development_dependency "minitest", [">= 0"]
-  b.gemspec.add_development_dependency  "buildar", ["~> 1.2"]
+                b.name = 'mudbug'
+    b.use_gemspec_file = true
+    b.use_version_file = true
+    b.version_filename = 'VERSION'
+             b.use_git = true
+  b.publish[:rubygems] = true
 end

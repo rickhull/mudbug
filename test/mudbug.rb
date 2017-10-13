edff729db7bd98a6cdbadcae1d195497a14be4a9
Rick Hull <rick.hull@gmail.com>
2017-10-13 20:53:20 +0000

    add response processing tests

--- test/mudbug.rb
+++ test/mudbug.rb
@@ -31,4 +31,55 @@ describe "Mudbug" do
       foo_ary[1].must_match qspec
     end
   end
+
+  describe "process" do
+    def http_response(body, opts = {})
+      opts[:http_ver] ||= 1.1
+      opts[:status_code] ||= '200 OK'
+      opts[:conn] ||= 'close'
+      headers = ["HTTP/#{opts[:http_ver]} #{opts[:status_code]}",
+                 "Content-Length: #{body.length}",
+                 "Connection: #{opts[:conn]}"]
+      headers << "Content-type: #{opts[:content_type]}" if opts[:content_type]
+      [headers, '', '', body].join("\n")
+    end
+
+    def dummy_io(str)
+      Net::BufferedIO.new(StringIO.new(str.gsub("\n", "\r\n")))
+    end
+
+    def net_http_resp(body, opts = {})
+      Net::HTTPResponse.read_new(dummy_io(http_response(body, opts)))
+    end
+
+    def rest_client_resp(body, opts = {})
+      req = RestClient::Request.new(method: 'get', url: 'http://google.com/')
+      RestClient::Response.create(body, net_http_resp(body, opts), req)
+    end
+
+    it "must process an empty response" do
+      empty_resp = rest_client_resp('', content_type: 'text/html')
+      body = Mudbug.process(empty_resp)
+      body.wont_be_nil
+      body.must_be_empty
+    end
+
+    it "must process a nonempty response" do
+      resp_body = 'foo: bar'
+      resp = rest_client_resp(resp_body, content_type: 'text/html')
+      body = Mudbug.process(resp)
+      body.wont_be_nil
+      body.wont_be_empty
+      body.must_equal resp_body
+    end
+
+    it "must warn about missing content-type" do
+      resp = rest_client_resp('')
+      out, err = capture_subprocess_io do
+        Mudbug.process(resp)
+      end
+      out.must_be_empty
+      err.wont_be_empty
+    end
+  end
 end
